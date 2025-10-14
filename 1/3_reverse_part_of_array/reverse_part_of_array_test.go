968a2bcb3379a1b7b638323d1a165adc341d0afa
Temir Balinov <balinov.tm21@physics.msu.ru>
2025-10-14 22:34:56 +0300

    feat(HW-1): Added solution to the reverse part of array problem.

---
+++ 1/3_reverse_part_of_array/reverse_part_of_array_test.go
@@ -0,0 +1,59 @@
+package reversepartofarray
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func Test_reversePartOfArray(t *testing.T) {
+	cases := []struct {
+		name string
+
+		array    []int
+		k        int
+		expected []int
+	}{
+		{
+			name: "1",
+
+			array:    []int{1, 2, 3, 4, 5, 6, 7},
+			k:        3,
+			expected: []int{5, 6, 7, 1, 2, 3, 4},
+		},
+		{
+			name: "2",
+
+			array:    []int{3, 3},
+			k:        0,
+			expected: []int{3, 3},
+		},
+		{
+			name: "3",
+
+			array:    []int{1, 2, 4},
+			k:        0,
+			expected: []int{2, 4, 1},
+		},
+		{
+			name: "4",
+
+			array:    []int{},
+			expected: []int{},
+		},
+		{
+			name: "5",
+
+			array:    []int{1},
+			expected: []int{1},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			reversePartOfArray(test.array, test.k)
+
+			assert.Equal(t, test.array, test.expected)
+		})
+	}
+}
