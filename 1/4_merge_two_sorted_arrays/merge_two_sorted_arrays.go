dc24c959b054a457ae6e903b7a2220b823c56712
Temir Balinov <balinov.tm21@physics.msu.ru>
2025-10-18 21:56:42 +0300

    feat(HW-1): Implement solution of merging two sorted arrays into a new sorted slice

--- 1/4_merge_two_sorted_arrays/merge_two_sorted_arrays.go
+++ 1/4_merge_two_sorted_arrays/merge_two_sorted_arrays.go
@@ -4,11 +4,30 @@ package merge2sortarray
 	Дано два отсортированных массива.
 
 	Необходимо написать функцию которая объединит эти два массива в один отсортированный.
-
-	p.s.: По условию не ясно какой вариант решать, буду решать тот что без аллокации
-	(с учетом того, что 1 массивчик проиннициализирован 0, там где )
 */
 
+// Решение 1
+func mergeWithNewSlice(arr1, arr2 []int) []int {
+	out := make([]int, 0)
+	i, j := 0, 0
+
+	for i < len(arr1) && j < len(arr2) {
+		if arr1[i] < arr2[j] {
+			out = append(out, arr1[i])
+			i++
+		} else {
+			out = append(out, arr2[j])
+			j++
+		}
+	}
+
+	out = append(out, arr1[i:]...)
+	out = append(out, arr2[j:]...)
+
+	return out
+}
+
+// Решение 2 (без доп аллокации)
 func merge(arr1, arr2 []int) {
 	writeIdx := len(arr1) - 1
 	idx1 := len(arr1) - len(arr2) - 1
--- 1/4_merge_two_sorted_arrays/merge_two_sorted_arrays_test.go
+++ 1/4_merge_two_sorted_arrays/merge_two_sorted_arrays_test.go
@@ -59,3 +59,57 @@ func Test_merge(t *testing.T) {
 		})
 	}
 }
+
+func Test_mergeWithNewSlice(t *testing.T) {
+	cases := []struct {
+		name string
+
+		array1   []int
+		array2   []int
+		expected []int
+	}{
+		{
+			name: "1",
+
+			array1:   []int{1, 3, 5, 7},
+			array2:   []int{2, 4, 6, 8},
+			expected: []int{1, 2, 3, 4, 5, 6, 7, 8},
+		},
+		{
+			name: "2",
+
+			array1:   []int{1, 3, 5, 7},
+			array2:   []int{-4, -3, -2, -1},
+			expected: []int{-4, -3, -2, -1, 1, 3, 5, 7},
+		},
+		{
+			name: "3",
+
+			array1:   []int{1, 2, 3, 4},
+			array2:   []int{-1, 5, 6},
+			expected: []int{-1, 1, 2, 3, 4, 5, 6},
+		},
+		{
+			name: "4",
+
+			array1:   []int{1, 2, 3, 4},
+			array2:   []int{},
+			expected: []int{1, 2, 3, 4},
+		},
+		{
+			name: "5",
+
+			array1:   []int{1},
+			array2:   []int{0, 1},
+			expected: []int{0, 1, 1},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.name, func(t *testing.T) {
+			actual := mergeWithNewSlice(test.array1, test.array2)
+
+			assert.Equal(t, actual, test.expected)
+		})
+	}
+}
